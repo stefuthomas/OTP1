fbe750932ea374f02e399bf9cbb5033326fdf363
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-09-03 10:05:13 +0300

    String manipulator task ready

--- src/main/java/StringManipulatorStefanos.java
+++ src/main/java/StringManipulatorStefanos.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class StringManipulatorStefanos {
+public class StringManipulatorStefanos {
+    public String reverse(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+    public String capitalize(String input) {
+        return input.toUpperCase();
+    }
 }
--- src/test/java/StringManipulatorStefanosTest.java
+++ src/test/java/StringManipulatorStefanosTest.java
@@ -1,4 +1,28 @@
+import org.junit.*;
+
 import static org.junit.jupiter.api.Assertions.*;
-class StringManipulatorStefanosTest {
-  
+
+public class StringManipulatorStefanosTest {
+
+    private static StringManipulatorStefanos stringManipulatorStefanos;
+
+    @Before
+    public void setUp() {
+        stringManipulatorStefanos = new StringManipulatorStefanos();
+    }
+
+    @Test
+    public void testReverse() {
+        assertEquals("Hello World", stringManipulatorStefanos.reverse("dlroW olleH"));
+    }
+
+    @Test
+    public void testCapitalize() {
+        assertEquals("HELLO", stringManipulatorStefanos.capitalize("hello"));
+    }
+
+    @After
+    public void tearDown() {
+        stringManipulatorStefanos = null;
+    }
 }
